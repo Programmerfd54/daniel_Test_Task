13402f5cc216d2653e1462e7aaf66d96a641c9d8
Programmerfd54 <99553384+Programmerfd54@users.noreply.github.com>
2023-12-02 01:12:03 +0300

    Update blocks.js

--- js/blocks.js
+++ js/blocks.js
@@ -1,13 +1,13 @@
- // Подключение шапки
- fetch('blocks/header.html')
+// Подключение шапки
+fetch('./blocks/header.html')
  .then(response => response.text())
  .then(data => {
      document.getElementById('headerContainer').innerHTML = data;
  });
 
 // Подключение футера
-fetch('blocks/footer.html')
+fetch('./blocks/footer.html')
  .then(response => response.text())
  .then(data => {
      document.getElementById('footerContainer').innerHTML = data;
- });    
+ });
